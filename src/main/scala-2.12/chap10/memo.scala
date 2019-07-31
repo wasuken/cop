fcc6feebd4f824ea260b11406151ce3b746d1d3e
wasuken <wevorence@gmail.com>
2019-07-31 16:39:29 +0900

    commit.

--- src/main/scala-2.12/chap10/memo.scala
+++ src/main/scala-2.12/chap10/memo.scala
@@ -9,16 +9,35 @@ object Element{
 abstract class Element{
   def contents: Array[String]
   def height: Int = contents.length
-  def width: Int =
-    if (height == 0) 0 else contents(0).length
-  def above(that: Element): Element =
-    Element.elem(this.contents ++ that.contents)
-  def beside(that: Element): Element =
+  def width: Int = contents(0).length()
+  def above(that: Element): Element = {
+    val this1 = this width that.width
+    val that1 = that width this.width
+    Element.elem(this1.contents ++ that1.contents)
+  }
+  def beside(that: Element): Element = {
+    val this1 = this heighten that.height
+    val that1 = that heighten this.height
     Element.elem(
       for(
-        (line1, line2) <- this.contents zip that.contents
+        (line1, line2) <- this.contents zip that1.contents
       ) yield line1 + line2
     )
+  }
+  def width(w: Int): Element =
+    if (w <= width) this
+    else {
+      val left = Element.elem(' ', (w - width) / 2, height)
+      val right = Element.elem(' ', w - width - left.width, height)
+      left beside this beside right
+    }
+  def heighten(h: Int): Element =
+    if (h <= height) this
+    else {
+      val top = Element.elem(' ', width, (h - height) / 2)
+      val bot = Element.elem(' ', width, h - height - top.height)
+      top above this above bot
+    }
   override def toString(): String = contents mkString "\n"
 }
 
@@ -38,4 +57,23 @@ class UniformElement (
 ) extends Element {
   private val line = ch.toString() * width
   def contents: Array[String] = Array.fill(height)(line)
+}
+object Spiral {
+  val space = Element.elem(" ")
+  val corner = Element.elem("+")
+  def spiral(nEdges: Int, direction: Int): Element = {
+    if (nEdges == 1)
+      Element.elem("+")
+    else{
+      val sp = spiral(nEdges - 1, (direction * 3) % 4)
+      def verticalBar = Element.elem('|', 1, sp.height)
+      def horizontalBar = Element.elem('-', sp.width, 1)
+      if (direction == 1){
+        (corner beside horizontalBar) above (sp beside space)
+      }else if(direction == 2){
+        (space beside sp) above (horizontalBar beside corner)
+      }else
+         (verticalBar above corner) beside (space above sp)
+    }
+  }
 }
